10495165a2797b6e094336b53b59948e38b10dc4
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-02-22 12:09:21 +0530

    added hero section ands css

--- shoekicks2.0/src/App.js
+++ shoekicks2.0/src/App.js
@@ -5,7 +5,15 @@ function App() {
   return (
     <>
     <Navbar />
-    <h1>4 baj gaye lekin party abhi baaki hai</h1>
+    
+    <section id="hero">
+        <h4>Trade-in-offer</h4>
+        <h2>Super Value Deals</h2>
+        <h1>On all products</h1>
+        <p>Save more with coupons upto 70% off!</p>
+        <button>Shop now</button>
+    </section>
+    
     </>
   );
 }
