ba1584e5683f5efdc25c02879a7a8eae9a47303c
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-02-22 11:56:17 +0530

    added images and navbar also

--- shoekicks2.0/src/App.js
+++ shoekicks2.0/src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./Components/Navbar";
 
 
 function App() {
   return (
     <>
+    <Navbar />
     <h1>4 baj gaye lekin party abhi baaki hai</h1>
     </>
   );
---
+++ shoekicks2.0/src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "../Media/CSS/style.css"
+import "../Media/images/site_logo.png"
+
+function Navbar() {
+  return (
+    <>
+      <section id="header">
+        <a href="/"> <img src={require("../Media/images/site_logo.png")} className="logo" alt="ShoeKicks" /> </a>
+        <div>
+          <ul id="navbar">
+            <li><a className="active" href="index.html">Home</a></li>
+            <li><a href="Shop.html">Shop</a></li>
+            <li><a href="Blog.html">Blog</a></li>
+            <li><a href="About.html">About</a></li>
+            <li><a href="contacts.html">Contacts</a></li>
+            <li><a href="Cart.html"><img src="images/shopping-bag.png" alt="" height="25px" /></a></li>
+          </ul>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Navbar
