9ee6b3e9cce9859407707b79ff1269ea72d44ae3
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-02-26 10:45:38 +0530

    added shop page and updated css

--- shoekicks2.0/src/Components/Shop.js
+++ shoekicks2.0/src/Components/Shop.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import "../Media/images/Nike_Avatars_Process-vimeo-442444722-hls-fastly_skyfire-2828.mp4"
 
 function Shop() {
   return (
-    <div>
-      <h1>this is Shop </h1>
-      <video src="https://www.nike.com/in/basketball/lebron-17" width={400}>
-      <source src="https://www.nike.com/in/basketball/lebron-17" type="video/mp4" />
+    <>
+    <div className='Shop'>
+
+      <video autoPlay loop muted playsInline className='backVideo' width={"100%"} height={"100vh"}>
+        <source src={require('../Media/images/Nike_Avatars_Process-vimeo-442444722-hls-fastly_skyfire-2828.mp4')} type='video/mp4'/>
       </video>
-      <h1>Helloooooooo</h1>
+
+        <button className='btnShop'>Explore</button>
+
     </div>
+    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo, velit ratione doloribus odit delectus perferendis?</h1>
+    </>
   )
 }
 
 export default Shop
+
