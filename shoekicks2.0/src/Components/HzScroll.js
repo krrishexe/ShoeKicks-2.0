ab8f10aa0484697f929d76eb5e73e2b8bda7ac56
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-03-08 15:51:04 +0530

    aadded hz scroll

--- shoekicks2.0/src/Components/HzScroll.js
+++ shoekicks2.0/src/Components/HzScroll.js
@@ -3,18 +3,38 @@ import HorizontalScroll from 'react-scroll-horizontal'
 
 function HzScroll() {
 
-    const child   = { width: `30em`, height: `100%`}
-    const parent  = { width: `60em`, height: `100%`}
+    // const child   = { width: `300em`, height: `100%`}
 
-  return (
-    <div style={parent}>
-        <HorizontalScroll>
-            <div style={child} />
-            <div style={child} />
-            <div style={child} />
-        </HorizontalScroll>
-      </div>
-  )
+    return (
+        <div className='Hz'>
+            <HorizontalScroll>
+                <div className="main bg" >
+                    <img src={require("../Media/images/shoes/lebron-17.jpg")} className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="main bg1" >
+                    <img src={require("../Media/images/shoes/lebron-17 (1).jpg")} className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="main bg2" >
+                    <img src={require("../Media/images/shoes/lebron-17 (2).jpg")} className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="main bg3" >
+                    <img src={require("../Media/images/shoes/lebron-17 (3).jpg")} className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="main bg4" >
+                    <img src={require("../Media/images/shoes/lebron-17 (4).jpg")} className="d-block w-100" alt="..." />
+                </div>
+
+                <div className="main bg5" >
+                    <img src={require("../Media/images/shoes/lebron-17 (5).jpg")} className="d-block w-100" alt="..." />
+                </div>
+
+            </HorizontalScroll>
+        </div>
+    )
 }
 
 export default HzScroll
