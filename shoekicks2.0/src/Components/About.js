24ef8fdd77c9de1c60d058299294ecc68f53eb07
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-03-27 23:21:42 +0530

    tried using grid

--- shoekicks2.0/src/Components/About.js
+++ shoekicks2.0/src/Components/About.js
@@ -3,11 +3,13 @@ import React from 'react'
 function About() {
   return (
     <div>
+
       <div class="container">
         <div class="big"></div>
         <div class="smol-1"></div>
         <div class="smol-2"></div>
       </div>
+      
     </div>
   )
 }
