0f9a52bcb86cabeeff184df4aa58cda078046638
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-03-29 20:00:27 +0530

    changed styles of about page

--- shoekicks2.0/src/Components/About.js
+++ shoekicks2.0/src/Components/About.js
@@ -3,13 +3,23 @@ import React from 'react'
 function About() {
   return (
     <div>
+      <div className="bigContainer">
 
       <div class="container">
         <div class="big"></div>
         <div class="smol-1"></div>
         <div class="smol-2"></div>
       </div>
+
+      <div className="test">
+        <h1>hellp</h1>
+      </div>
       
+
+
+
+      
+      </div>
     </div>
   )
 }
