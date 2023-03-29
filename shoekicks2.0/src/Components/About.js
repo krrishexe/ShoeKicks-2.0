048ca83f7c50516e988a7a34a715226369bfc443
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-03-29 20:23:54 +0530

    Added lottie anim in webp

--- shoekicks2.0/src/Components/About.js
+++ shoekicks2.0/src/Components/About.js
@@ -4,24 +4,31 @@ function About() {
   return (
     <div>
       <div className="bigContainer">
+        <div className="test" style={{ textAlign: 'center', margin: "50px" }}>
+          <h2>Empty your pocket</h2>
+          <picture>
+            <source srcSet="https://fonts.gstatic.com/s/e/notoemoji/latest/1f4b8/512.webp" type="image/webp" />
+              <img src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f4b8/512.gif" alt="💸" width="32" height="32" />
+          </picture>
+        </div>
 
-      <div class="container">
-        <div class="big"></div>
-        <div class="smol-1"></div>
-        <div class="smol-2"></div>
+            <div class="container">
+              <div class="big"></div>
+              <div class="smol-1"></div>
+              <div class="smol-2"></div>
+            </div>
+
+            <div className="test" style={{ textAlign: 'center', margin: "50px" }}>
+              <h2>New Arrivals</h2>
+            </div>
+
+
+
+
+
+        </div>
       </div>
-
-      <div className="test">
-        <h1>hellp</h1>
-      </div>
-      
-
-
-
-      
-      </div>
-    </div>
-  )
+      )
 }
 
-export default About
+      export default About
