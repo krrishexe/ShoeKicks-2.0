efe534678154d440570cc2091e312a2c902667bb
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-04 22:25:25 +0530

    added map function in shop component

--- shoekicks2.0/src/Components/Shoe.js
+++ shoekicks2.0/src/Components/Shoe.js
@@ -2,35 +2,53 @@ import React, { useState, useEffect } from 'react'
 import ShoeItem from './ShoeItem'
 
 const Shoe = (props) => {
-    const [data, setData] = useState(null);
-    // const [loading, setLoading] = useState(true);
-    // const [error, setError] = useState(null);
-
+    const [articles, setArticles] = useState([]);
 
 
     useEffect(() => {
-        fetch('http://localhost:5000/posts/', {
+
+        fetchMoreData();
+
+
+        // fetch('http://localhost:5000/posts/', {
+        //     mode: 'no-cors',
+        //     method: 'GET',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     }
+        // })
+        // .then(response => response.json())
+        // .then((json) => {
+        //         console.log(json.images);
+                // setLoading(false);
+                // setData(json);
+        //     })
+    })
+
+    const fetchMoreData = async () => {
+        const url = `http://localhost:5000/posts`;
+        let data = await fetch(url,{
             mode: 'no-cors',
-            method: 'POST',
+            method: 'GET',
             headers: {
                 'content-type': 'application/json'
             }
-        })
-        .then(response => response.json())
-        .then((json) => {
-                console.log(json.images);
-                // setLoading(false);
-                // setData(json);
-            })
-    }, [])
-
+        });
+        let parsedData = await data.json()
+        setArticles(articles.concat(parsedData.articles))
+        // setTotalResults(parsedData.totalResults)
+    };
 
     return (
         <div>
             <div className="App">
-                <ShoeItem />
-                {/* {loading && <p>Loading...</p>}
-                {!loading && <p>Fetched data</p>} */}
+            <div className="row">
+                {articles.map((element) => {
+                    return <div>
+                        <ShoeItem vendor={element.vendor ? element.vendor : ""} name={element.name ? element.name : ""} images={element.images} source={element.source.name} />
+                    </div>
+                })}
+            </div> 
             </div>
 
 
@@ -41,10 +59,3 @@ const Shoe = (props) => {
 export default Shoe
 
 
-{/* <div className="row">
-                {articles.map((element) => {
-                    return <div className="col-md-4" key={element.link}>
-                        <ShoeItem vendor={element.vendor ? element.vendor : ""} name={element.name ? element.name : ""} images={element.images} source={element.source.name} />
-                    </div>
-                })}
-            </div> */}
