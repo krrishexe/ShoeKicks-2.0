1f3b58289de761bfb0764da36433cd1cee213c12
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-08 10:44:37 +0530

    added a empty skeleton container

--- shoekicks2.0/src/Components/Shoe.js
+++ shoekicks2.0/src/Components/Shoe.js
@@ -1,51 +1,75 @@
 import React, { useState, useEffect } from 'react'
 import ShoeItem from './ShoeItem'
-import Skeleton from 'react-loading-skeleton';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const Shoe = (props) => {
     const [products, setProducts] = useState([]);
-    const [loading,setLoading]= useState(true)
+    const [loading, setLoading] = useState(true)
 
 
     useEffect(() => {
 
         setTimeout(() => {
-            
+
             fetchMoreData();
         }, 2000);
     })
 
     const fetchMoreData = async () => {
         const url = `http://localhost:5000/posts/`;
-        let data = await fetch(url,{
+        let data = await fetch(url, {
             mode: 'cors',
             method: 'GET',
             headers: {
                 'content-type': 'application/json'
-                }
+            }
         });
         let parsedData = await data.json()
         // console.log(parsedData)
         // console.log(parsedData[0])
         setProducts(products.concat(parsedData[0].products))
-    
+
         // setTotalResults(parsedData.totalResults)
     };
 
     return (
         <div>
             <div className="App">
-                <ShoeItem />
+                <SkeletonTheme baseColor="#202020" highlightColor="#444">
+                <div class="product-card" >
+                    <div class="badge">Hot</div>
+                    <div class="product-tumb">
+                        <Skeleton  height={303} width={380} /> 
+                    </div>
+                    <div class="product-details">
+                        <span class="product-catagory"> <Skeleton /> </span>
+                        <h4><a href="#"> <Skeleton /> </a></h4>
+                        {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, possimus nostrum!</p> */}
+                        <div class="product-bottom-details">
+                            <div class="product-price"><Skeleton/> </div>
+                            <div class="product-links">
+                                <a href="#"><i class="fa fa-heart"></i></a>
+                                <a href="#"><i class="fa fa-shopping-cart"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                {products.map((element) => {
+                </SkeletonTheme>
+            </div>
+
+            <ShoeItem />
+
+            {/* {products.map((element) => {
                     return <div key={element.url}>
                         <ShoeItem vendor={element.vendor ? element.vendor : ""} name={element.name ? element.name : ""} images={element.images} price={element.price} />
                     </div>
-                })}
-            </div>
-
-
+                })} */}
         </div>
+
+
+       
     )
 }
 
