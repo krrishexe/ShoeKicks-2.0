f7fefd9c1be4c831d7e3da2fd4a3ff27f874d6e2
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-12 22:32:43 +0530

    added loading anim and added scroll logic

--- shoekicks2.0/src/Components/Shoe.js
+++ shoekicks2.0/src/Components/Shoe.js
@@ -7,6 +7,9 @@ const Shoe = (props) => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true)
 
+    let page = 1;
+
+    let past_height = 0;
 
     useEffect(() => {
 
@@ -14,7 +17,45 @@ const Shoe = (props) => {
 
             fetchMoreData();
         }, 2000);
-    },[])
+
+
+        window.addEventListener('scroll', () => {
+
+            const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+            // console.log({ scrollTop, scrollHeight, clientHeight });
+
+            console.log(past_height, scrollHeight)
+                    if (clientHeight + scrollTop >= scrollHeight - 200) {
+                        console.log("to the bottom");
+                        // show the loading animation once 
+
+                        if (scrollHeight > past_height){
+                        
+                            showLoading();
+                            past_height = scrollHeight;
+                        }
+                    }
+        
+        })
+        const loading = document.querySelector('.loading')
+        
+        function showLoading() {
+            // animation visible 
+            loading.classList.add('show');
+            // loading more data once it reaches to the bottom
+            
+            setTimeout(() => {
+                
+                fetchMoreData();
+                loading.classList.remove('show');
+                
+            }, 1000);
+
+        }
+
+
+    }, [])
+    
 
     const fetchMoreData = async () => {
         const url = `http://localhost:5000/posts/`;
@@ -26,12 +67,13 @@ const Shoe = (props) => {
             }
         });
         let parsedData = await data.json()
-        // console.log(parsedData)
         // console.log(parsedData[0])
-        setProducts((parsedData[0].products))
-        setLoading(false)
 
-        // setTotalResults(parsedData.totalResults)
+
+        setProducts((parsedData[0].products).slice(0,page * 6))
+        page+=1;
+        setLoading(false)
+       
     };
 
     return (
@@ -52,12 +94,12 @@ const Shoe = (props) => {
                                             </div>
                                             <div class="product-details">
                                                 <span class="product-catagory"> <Skeleton /> </span>
-                                                <h4><a href="#"> <Skeleton /> </a></h4>
+                                                <h4><a href="/"> <Skeleton /> </a></h4>
                                                 <div class="product-bottom-details">
                                                     <div class="product-price"><Skeleton /> </div>
                                                     <div class="product-links">
-                                                        <a href="#"><i class="fa fa-heart"></i></a>
-                                                        <a href="#"><i class="fa fa-shopping-cart"></i></a>
+                                                        <a href="/"><i class="fa fa-heart"></i></a>
+                                                        <a href="/"><i class="fa fa-shopping-cart"></i></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -73,12 +115,12 @@ const Shoe = (props) => {
                                             </div>
                                             <div class="product-details">
                                                 <span class="product-catagory"> <Skeleton /> </span>
-                                                <h4><a href="#"> <Skeleton /> </a></h4>
+                                                <h4><a href="/"> <Skeleton /> </a></h4>
                                                 <div class="product-bottom-details">
                                                     <div class="product-price"><Skeleton /> </div>
                                                     <div class="product-links">
-                                                        <a href="#"><i class="fa fa-heart"></i></a>
-                                                        <a href="#"><i class="fa fa-shopping-cart"></i></a>
+                                                        <a href="/"><i class="fa fa-heart"></i></a>
+                                                        <a href="/"><i class="fa fa-shopping-cart"></i></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -94,12 +136,12 @@ const Shoe = (props) => {
                                             </div>
                                             <div class="product-details">
                                                 <span class="product-catagory"> <Skeleton /> </span>
-                                                <h4><a href="#"> <Skeleton /> </a></h4>
+                                                <h4><a href="/"> <Skeleton /> </a></h4>
                                                 <div class="product-bottom-details">
                                                     <div class="product-price"><Skeleton /> </div>
                                                     <div class="product-links">
-                                                        <a href="#"><i class="fa fa-heart"></i></a>
-                                                        <a href="#"><i class="fa fa-shopping-cart"></i></a>
+                                                        <a href="/"><i class="fa fa-heart"></i></a>
+                                                        <a href="/"><i class="fa fa-shopping-cart"></i></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -115,12 +157,12 @@ const Shoe = (props) => {
                                             </div>
                                             <div class="product-details">
                                                 <span class="product-catagory"> <Skeleton /> </span>
-                                                <h4><a href="#"> <Skeleton /> </a></h4>
+                                                <h4><a href="/"> <Skeleton /> </a></h4>
                                                 <div class="product-bottom-details">
                                                     <div class="product-price"><Skeleton /> </div>
                                                     <div class="product-links">
-                                                        <a href="#"><i class="fa fa-heart"></i></a>
-                                                        <a href="#"><i class="fa fa-shopping-cart"></i></a>
+                                                        <a href="/"><i class="fa fa-heart"></i></a>
+                                                        <a href="/"><i class="fa fa-shopping-cart"></i></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -136,12 +178,12 @@ const Shoe = (props) => {
                                             </div>
                                             <div class="product-details">
                                                 <span class="product-catagory"> <Skeleton /> </span>
-                                                <h4><a href="#"> <Skeleton /> </a></h4>
+                                                <h4><a href="/"> <Skeleton /> </a></h4>
                                                 <div class="product-bottom-details">
                                                     <div class="product-price"><Skeleton /> </div>
                                                     <div class="product-links">
-                                                        <a href="#"><i class="fa fa-heart"></i></a>
-                                                        <a href="#"><i class="fa fa-shopping-cart"></i></a>
+                                                        <a href="/"><i class="fa fa-heart"></i></a>
+                                                        <a href="/"><i class="fa fa-shopping-cart"></i></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -157,12 +199,12 @@ const Shoe = (props) => {
                                             </div>
                                             <div class="product-details">
                                                 <span class="product-catagory"> <Skeleton /> </span>
-                                                <h4><a href="#"> <Skeleton /> </a></h4>
+                                                <h4><a href="/"> <Skeleton /> </a></h4>
                                                 <div class="product-bottom-details">
                                                     <div class="product-price"><Skeleton /> </div>
                                                     <div class="product-links">
-                                                        <a href="#"><i class="fa fa-heart"></i></a>
-                                                        <a href="#"><i class="fa fa-shopping-cart"></i></a>
+                                                        <a href="/"><i class="fa fa-heart"></i></a>
+                                                        <a href="/"><i class="fa fa-shopping-cart"></i></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -176,9 +218,15 @@ const Shoe = (props) => {
                                 return <div key={element.url} className='bigCard'>
                                     <ShoeItem vendor={element.vendor ? element.vendor : ""} name={element.name ? element.name : ""} images0={element.images[0]} images1={element.images[1]} price={element.price} />
                                 </div>
-                            }).slice(0,6)
+                            })
                     }
                 </SkeletonTheme>
+
+                <div class="loading">
+                    <div class="ball"></div>
+                    <div class="ball"></div>
+                    <div class="ball"></div>
+                </div>
             </div>
 
 
