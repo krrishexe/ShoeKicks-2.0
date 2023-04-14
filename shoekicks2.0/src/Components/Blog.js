0a0620185e38069f933ba9ea8082993f069b04cb
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-14 20:35:21 +0530

    tried shoedetails in blog page

--- shoekicks2.0/src/Components/Blog.js
+++ shoekicks2.0/src/Components/Blog.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 function Blog() {
   return (
-    <div>
-      <h1>Hello this is Blog page</h1>
-      
+    <div className='twoFlex'>
+
+      <div>
+        <div className="images-section parent">
+          <div className="twoFlex">
+            <img src="../Media/images/f1.jpg" alt="" height={"300px"} width={"300px"} />
+            <img src="../Media/images/f2-1.png" alt="" height={"300px"} width={"300px"} />
+          </div>
+          <div className="twoFlex">
+            <img src="../Media/images/f3-2.jpg" alt="" height={"300px"} width={"300px"} />
+            <img src="../Media/images/f4-1.jpg" alt="" height={"300px"} width={"300px"} />
+          </div>
+        </div>
+        <div className="articles-desc">
+
+          <div className='twoFlex'>
+            <span>NIKE</span>
+            <i>heart</i>
+          </div>
+
+          <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum!</h2>
+
+          <div className='twoFlex'>
+            <h2 className='price'></h2>
+            <span>Size Chart</span>
+          </div>
+
+          <div>
+            <button type='checkbox'></button>
+            <button type='checkbox'></button>
+            <button type='checkbox'></button>
+            <button type='checkbox'></button>
+            <button type='checkbox'></button>
+          </div>
+
+          <div>
+            <button>Add to cart</button>
+          </div>
+
+        </div>
+      </div>
 
     </div>
   )
