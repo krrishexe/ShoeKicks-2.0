cc0a2ec62aa8bd8a26e2e075a91520fcc99cc76e
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-26 11:07:35 +0530

    working on actions

---
+++ shoekicks2.0/src/Services/Actions/Action.js
@@ -0,0 +1,17 @@
+import {ADD_TO_CART} from '../Constants'
+import {REMOVE_FROM_CART} from '../Constants'
+
+export const addToCart =(data)=>{
+    return {
+        data:data,
+        type:ADD_TO_CART,
+    }
+}
+
+export const removeFromCart =(data)=>{
+    return {
+        data:data,
+        type:REMOVE_FROM_CART
+    }
+}
+
