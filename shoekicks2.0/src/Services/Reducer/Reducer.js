f66c5405dba5bab85537d610215fbb1ca21326ef
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-26 12:49:56 +0530

    updated reducer and rootreducer and used combineReducer function from redux

--- shoekicks2.0/src/Services/Reducer/Reducer.js
+++ shoekicks2.0/src/Services/Reducer/Reducer.js
@@ -1,1 +1,24 @@
-export default function
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../Constants"
+
+const initialState ={
+    cartData:[]
+}
+const state = initialState
+export default function items(initialState,action){
+
+    switch(action.type){
+        case ADD_TO_CART :
+            return{
+                ...state ,
+                cartData:action.data
+            }
+            break
+        case REMOVE_FROM_CART :
+            return{
+                ...state,
+                cartData:action.data
+            }
+        default :
+            return state
+    }
+}
