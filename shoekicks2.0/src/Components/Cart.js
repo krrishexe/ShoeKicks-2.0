e062ea9539de44c7d28a89bbb34ba921e8024542
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-05-01 23:11:50 +0530

    added cartitem component

--- shoekicks2.0/src/Components/Cart.js
+++ shoekicks2.0/src/Components/Cart.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+import CartItem from './CartItem'
 
 function Cart() {
+  const [data, setData] = useState(false)
   return (
     <div>
-      <h1>this is cart Component</h1>
+      <div className="cart-content">
+        
+      {
+        data ? <>
+                  <h2 className='tcCenter'>Cart is Empty : (</h2>
+                  <button className='btnShop1' >Browse Products</button>
+              </> 
+         : 
+              <>
+              <CartItem /> 
+              <div className="cart-footer">
+                <div className="sub-total">
+                  <span className='text'>Subtotal :</span>
+                  <span className='text total'>&#8377; 1234</span>
+                </div>
+                <div className="btn">
+                  <button className='checkout-cta'>CheckOut</button>
+                </div>
+              </div>
+              </>
+      
+      }
+      </div>
     </div>
   )
 }
---
+++ shoekicks2.0/src/Components/CartItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CartItem() {
+  return (
+    <div>
+      <h1>CartItem</h1>
+    </div>
+  )
+}
+
+export default CartItem
