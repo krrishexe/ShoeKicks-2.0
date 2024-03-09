4c97fb85c5f8f48a53a0b1c8f28d398ae4d7501e
Krish Yadav <75037831+krrishexe@users.noreply.github.com>
2024-03-09 14:27:27 +0530

    added brand icons and css

--- shoekicks2.0/src/Components/Shoes.js
+++ shoekicks2.0/src/Components/Shoes.js
@@ -4,6 +4,15 @@ import RelatedProducts from './RelatedProducts'
 import UserContext from '../Context/UserContext'
 import { useContext } from 'react'
 import { useParams } from 'react-router-dom'
+import { SiNike } from "react-icons/si";
+import { SiAdidas } from "react-icons/si";
+import { SiPuma } from "react-icons/si";
+import { SiJordan } from "react-icons/si";
+import { GiConverseShoe } from "react-icons/gi";
+import { SiReebok } from "react-icons/si";
+
+
+
 
 function Shoes() {
   const [particularData, setParticularData] = useState([])
@@ -34,57 +43,86 @@ function Shoes() {
       {
         particularData.map((item) => {
           return (
-          <div key={item.id}>
-            <h2 className='bigText tcCenter'>{item.name}</h2>
+            <>
 
-            <div className='twoFlex'>
-
-              <div className="images-section parent">
-                <div className="spacey">
-                  <img className='spacey-bw' src={item.images[0]} alt="" height={"400px"} width={"400px"} />
-                  <img className='spacey-bw' src={item.images[1]} alt="" height={"400px"} width={"400px"} />
-                </div>
-                <div className="spacey">
-                  <img className='spacey-bw' src={item.images[2]} alt="" height={"400px"} width={"400px"} />
-                  <img className='spacey-bw' src={item.images[3]} alt="" height={"400px"} width={"400px"} />
-                </div>
-              </div>
-              <div className="articles-desc">
-
-                <div className='twoFlex'>
-                  <span className='vendor'>NIKE &gt; {item.vendor}</span>
-                  <AiOutlineHeart className='vendor' />
-                </div>
-
-                <h2>{item.name}</h2>
-                <p className='descc'>{item.description}</p>
-
-                <div className='twoFlex'>
-                  <h3 className='vendor'>{item.price}</h3>
-                  <span className='vendor'>shoe Type </span>
-                </div>
-
-                <div>
-                  <button type='checkbox' className='checkbox'>1</button>
-                  <button type='checkbox' className='checkbox'>2</button>
-                  <button type='checkbox' className='checkbox'>3</button>
-                  <button type='checkbox' className='checkbox'>4</button>
-                  <button type='checkbox' className='checkbox'>5</button>
-                </div>
-                <div className='quantity-buttons'>
-                  <span>-</span>
-                  <span>5</span>
-                  <span>+</span>
+              <div className="container1">
+                <div className="row">
+                  <div className="col-12">
+                    <nav aria-label="breadcrumb">
+                      <ol className="breadcrumb">
+                        <li className="breadcrumb-item"><a href="#"><i className="fa fa-home" /> Home</a></li>
+                        <li className="breadcrumb-item"><a href="#">Shop</a></li>
+                        <li className="breadcrumb-item active" aria-current="page">Product Details</li>
+                      </ol>
+                    </nav>
+                  </div>
                 </div>
                 <div>
-                  <button className='add-to-cart'>Add to cart <AiOutlineShoppingCart /> </button>
+                  <h1 className='tcCenter'>ShoeKick Original's</h1>
+                  <br />
                 </div>
-
               </div>
+              <div key={item.id}>
 
-            </div>
-            <RelatedProducts />
-          </div>)
+
+                <div className='twoFlex'>
+
+                  <div className="images-section parent">
+                    <div className="spacey">
+                      <img className='spacey-bw' src={item.images[0]} alt="" height={"400px"} width={"400px"} />
+                      <img className='spacey-bw' src={item.images[1]} alt="" height={"400px"} width={"400px"} />
+                    </div>
+                    <div className="spacey">
+                      <img className='spacey-bw' src={item.images[2]} alt="" height={"400px"} width={"400px"} />
+                      <img className='spacey-bw' src={item.images[3]} alt="" height={"400px"} width={"400px"} />
+                    </div>
+                  </div>
+                  <div className="articles-desc">
+
+                    <div className='vendor-name'>
+                      <span className='vendor'>{
+                        item.vendor.includes('Nike') ? <div> <SiNike /> <p>{item.vendor}</p></div> :
+                          item.vendor.includes('Adidas') ? <div> <SiAdidas /> {item.vendor}</div> :
+                            item.vendor.includes('Puma') ? <div> <SiPuma /> {item.vendor}</div> :
+                            item.vendor.includes('Reebok') ? <div> <SiReebok /> {item.vendor}</div> :
+                              item.vendor.includes('Jordan') ? <div> <SiJordan /> {item.vendor}</div> : <div>
+                                <GiConverseShoe /> {item.vendor}
+                              </div>
+                      }</span>
+
+                    </div>
+
+                    <h2 className='genos'>{item.name}</h2>
+                    <p className='descc'>{item.description}</p>
+
+                    <div className='twoFlex'>
+                      <h3 className='vendor'>{item.price}</h3>
+                      <span className='vendor'>shoe Type </span>
+                    </div>
+
+                    <div>
+                      <button type='checkbox' className='checkbox'>1</button>
+                      <button type='checkbox' className='checkbox'>2</button>
+                      <button type='checkbox' className='checkbox'>3</button>
+                      <button type='checkbox' className='checkbox'>4</button>
+                      <button type='checkbox' className='checkbox'>5</button>
+                    </div>
+                    <div className='quantity-buttons'>
+                      <span>-</span>
+                      <span>5</span>
+                      <span>+</span>
+                    </div>
+                    <div>
+                      <button className='add-to-cart'>Add to cart <AiOutlineShoppingCart /> </button>
+                    </div>
+
+                  </div>
+
+                </div>
+                <RelatedProducts />
+              </div>
+            </>
+          )
         })
       }
     </>
