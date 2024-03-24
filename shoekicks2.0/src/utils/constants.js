6324fe39e9bd1839b2057db52a243f9c6a7e4ba7
Krish Yadav <75037831+krrishexe@users.noreply.github.com>
2024-03-25 05:21:17 +0530

    added dns

--- shoekicks2.0/src/utils/constants.js
+++ shoekicks2.0/src/utils/constants.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://34.131.191.125:5000/api/v1';
+const BASE_URL = 'http://krish.social:5000/api/v1';
 // const BASE_URL = 'http://localhost:5000/api/v1';
 
 export { BASE_URL };
