502f8d35648e056bdf939ccca545c1249df4e7e0
Krish Yadav <75037831+krrishexe@users.noreply.github.com>
2024-06-28 14:40:58 +0530

    Update constants.js

--- shoekicks2.0/src/utils/constants.js
+++ shoekicks2.0/src/utils/constants.js
@@ -1,5 +1,5 @@
 // const BASE_URL = '';
-const BASE_URL = 'http://krish.social:5000/api/v1';
+const BASE_URL = 'https://krish.social/api/v1/posts';
 // const BASE_URL = 'http://localhost:5000/api/v1';
 
 export { BASE_URL };
